41b3460a6867b544193c2ff46c56c29af8ec3a48
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-10-29 15:55:03 +0530

    working fine

--- OOD_gfg/ECom/MyECom.js
+++ OOD_gfg/ECom/MyECom.js
@@ -8,17 +8,24 @@ const  MyECom =  {
     productsCatalog : new ProductsCatalog()
 }
 
-const customer = new Customer("Alice", "1234567890","Nagpur");
-const seller = new Seller("Bob's Electronics", "0987654321");
+const customer1 = new Customer("Kiran Test 1", 99999999, "Keshori")
+const seller1 = new Seller("Seller Test 1", 8888888888);
 
-const electronics = new Category("Electronics", "Electronic items");
-const camera = new Product("Camera", "Digital Camera", 500, electronics, 10);
+const electronics = new Category("Electronics", "Electronincs items");
+// name, description, price, category, availableCount
+const icamera = new Product("Iphone Camera", "12MP Digital Camera", 129000, electronics, 100);
+const myCamera = new Product("MyPhone Camera", "120MP Digital Camera", 29000, electronics, 100);
 
-seller.registerProduct(MyECom.productsCatalog, camera);
+seller1.registerProduct(MyECom.productsCatalog, icamera)
+seller1.registerProduct(MyECom.productsCatalog, myCamera)
 
-const foundProduct = MyECom.productsCatalog.searchProduct("camera");
-customer.addItemToCart({product: camera, count: 2});
-customer.placeOrder();
+const foundProduct1 = MyECom.productsCatalog.searchProduct("Iphone Camera")
+const foundProduct2 = MyECom.productsCatalog.searchProduct("MyPhone Camera")
 
-console.log(foundProduct);
-console.log(customer.getOrderHistory());
+console.log(foundProduct1)
+console.log(foundProduct2)
+
+customer1.placeOrder()
+
+console.log(customer1.getOrderHistory())
+
